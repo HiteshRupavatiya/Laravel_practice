652ed77c5879f18cfea4e7a82eef1261882075f3
HiteshRupavatiya <hitesh@zignuts.com>
2023-02-22 14:52:26 +0530

    has one through relationship defined in Mechanic model using Car model

--- relationship_practice/app/Models/Mechanic.php
+++ relationship_practice/app/Models/Mechanic.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Mechanic extends Model
 {
     use HasFactory;
+
+    public function carOwner()
+    {
+        return $this->hasOneThrough(Owner::class, Car::class);
+        // return $this->through('cars')->has('owner'); // String Syntax of Relationships
+        // return $this->throughCars()->hasOwner(); // Dynamic Syntax of Relationships
+    }
 }
