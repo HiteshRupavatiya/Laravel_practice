7494ee15a34b9f755b1cffbe7690e51236a1abb1
HiteshRupavatiya <hitesh@zignuts.com>
2023-02-22 11:58:22 +0530

    one to one relationship complete

--- relationship_practice/app/Http/Controllers/PhoneController.php
+++ relationship_practice/app/Http/Controllers/PhoneController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Phone;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PhoneController extends Controller
 {
-    //
+    public function getUserOfPhone($id)
+    {
+        $user = User::find($id)->phone;
+        return $user;
+    }
+
+    public function getPhoneOfUser($id)
+    {
+        $phone = Phone::find($id)->user;
+        return $phone;
+    }
 }
--- relationship_practice/routes/web.php
+++ relationship_practice/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PhoneController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('findUser/{id}', [PhoneController::class, 'getUserOfPhone'])->name('phone.user');
+
+Route::get('findPhone/{id}', [PhoneController::class, 'getPhoneOfUser'])->name('user.phone');
