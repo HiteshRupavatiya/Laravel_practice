ce575b442d368bc698b25bb7f37e127f5792ca98
HiteshRupavatiya <hitesh@zignuts.com>
2023-02-23 11:48:08 +0530

    polymorphic One to One relationship complete

--- polymorphic_relationship/app/Http/Controllers/UserController.php
+++ polymorphic_relationship/app/Http/Controllers/UserController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function getPostImage($id){
+        $image = Post::find($id)->image;
+        return $image;
+    }
+
+    public function getUserImage($id){
+        $user = User::find($id)->image;
+        return $user;
+    }
 }
