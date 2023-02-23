f2fbaa35cab7f2836b30ec311d1d5cbe5cf0963a
HiteshRupavatiya <hitesh@zignuts.com>
2023-02-23 12:13:25 +0530

    videos, comments migration file modified and column like title, url added in videos and body, commentable_id, commentable_type in comments

--- polymorphic_relationship/database/migrations/2023_02_23_063658_create_comments_table.php
+++ polymorphic_relationship/database/migrations/2023_02_23_063658_create_comments_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('body');
+            $table->integer('commentable_id');
+            $table->string('commentable_type');
             $table->timestamps();
         });
     }
